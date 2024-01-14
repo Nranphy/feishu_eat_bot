0a7f31e77bc151345b87188445557978f8e9f073
Nranphy <3102002900@qq.com>
2024-01-14 13:17:10 +0800

    Style: 修改了默认占位的菜品格式。

--- code/main.py
+++ code/main.py
@@ -40,7 +40,7 @@ def eat_remind(time_segment: str = ""):
         logger.debug("本日不是工作日，已忽略任务...")
         return
     with open(data_path / "what2eat.json", "r", encoding="utf-8-sig") as f:
-        dish_info = json.load(f)
+        dish_info = json.load(f).get("data")
     dish_choose = random.choices(dish_info, k = 5)
     dish_text = "\n".join(
         [f"{i+1}. 【{dish.get('type')}】{dish.get('name')} 家的\n\
@@ -54,11 +54,11 @@ def eat_remind(time_segment: str = ""):
                     "要准备吃饭了吗~\n"
                     "\n"
                     "如果不知道吃什么，那么现在推荐的菜品有——\n"
-                    "1. 水煮大蝎子走路菇火锅！！\n"
-                    "2. 食人植物水果塔\n"
-                    "3. 炸什锦曼德拉草与大蝙蝠天妇罗\n"
-                    "4. 会动的铠甲全餐\n"
-                    "5. 祈求消灾解厄！除灵雪酪\n"
+                    "1. 【堂食】迷宫饭 家的\n水煮大蝎子走路菇火锅！！  (￥0)\n"
+                    "2. 【堂食】迷宫饭 家的\n食人植物水果塔  (￥0)\n"
+                    "3. 【堂食】迷宫饭 家的\n炸什锦曼德拉草与大蝙蝠天妇罗  (￥0)\n"
+                    "4. 【堂食】迷宫饭 家的\n会动的铠甲全餐  (￥0)\n"
+                    "5. 【堂食】迷宫饭 家的\n祈求消灾解厄！除灵雪酪  (￥0)\n"
                     # f"{dish_text}"
                     "\n"
                     f"那么，今天{time_segment}也要好好干饭哦~！！"
