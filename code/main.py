971042e48dfb71fc76d4c19a083ee455392ead06
Nranphy <3102002900@qq.com>
2024-01-14 18:31:44 +0800

    Feat: 上下班打卡现在会 at 所有人了。

--- code/main.py
+++ code/main.py
@@ -19,14 +19,14 @@ bot = Bot(
     webhook_url
 )
 
-bot.send(
-    Message([
-        MessageSegment.text("*测试*"),
-        MessageSegment.delimiter(),
-        MessageSegment.text("第二**段**文本"),
-        MessageSegment.delimiter(),
-        ], title="这是一个测试", type="text")
-)
+# bot.send(
+#     Message([
+#         MessageSegment.text("*测试*"),
+#         MessageSegment.delimiter(),
+#         MessageSegment.text("第二**段**文本"),
+#         MessageSegment.delimiter(),
+#         ], title="这是一个测试", type="text")
+# )
 
 
 @schedule.repeat(schedule.every().day.at("11:50"), time_segment = "中午")
@@ -49,21 +49,21 @@ def eat_remind(time_segment: str = ""):
     bot.send(
         Message(
                 [
-                MessageSegment.text(
-                    f"已经是{time_segment}{get_china_time()}了，\n"
-                    "要准备吃饭了吗~\n"
-                    "\n"
-                    "如果不知道吃什么，那么现在推荐的菜品有——\n"
-                    "1. 【堂食】迷宫饭 家的\n水煮大蝎子走路菇火锅！！  (￥0)\n"
-                    "2. 【堂食】迷宫饭 家的\n食人植物水果塔  (￥0)\n"
-                    "3. 【堂食】迷宫饭 家的\n炸什锦曼德拉草与大蝙蝠天妇罗  (￥0)\n"
-                    "4. 【堂食】迷宫饭 家的\n会动的铠甲全餐  (￥0)\n"
-                    "5. 【堂食】迷宫饭 家的\n祈求消灾解厄！除灵雪酪  (￥0)\n"
-                    # f"{dish_text}"
-                    "\n"
-                    f"那么，今天{time_segment}也要好好干饭哦~！！"
-                )
-            ],
+                    MessageSegment.text(
+                        f"已经是{time_segment}{get_china_time()}了，\n"
+                        "要准备吃饭了吗~\n"
+                        "\n"
+                        "如果不知道吃什么，那么现在推荐的菜品有——\n"
+                        "1. 【堂食】迷宫饭 家的\n水煮大蝎子走路菇火锅！！  (￥0)\n"
+                        "2. 【堂食】迷宫饭 家的\n食人植物水果塔  (￥0)\n"
+                        "3. 【堂食】迷宫饭 家的\n炸什锦曼德拉草与大蝙蝠天妇罗  (￥0)\n"
+                        "4. 【堂食】迷宫饭 家的\n会动的铠甲全餐  (￥0)\n"
+                        "5. 【堂食】迷宫饭 家的\n祈求消灾解厄！除灵雪酪  (￥0)\n"
+                        # f"{dish_text}"
+                        "\n"
+                        f"那么，今天{time_segment}也要好好干饭哦~！！"
+                    )
+                ],
             title = f"{time_segment}的干饭提醒~"
         )
     )
@@ -80,13 +80,15 @@ def start_remind():
     bot.send(
         Message(
                 [
-                MessageSegment.text(
-                    f"现在是{get_china_time()}，\n"
-                    f"要准备开始今天的工作了！！\n"
-                    "\n"
-                    "去工位前要记得打卡哦~"
-                )
-            ],
+                    MessageSegment.at("all"),
+                    MessageSegment.delimiter(),
+                    MessageSegment.text(
+                        f"现在是{get_china_time()}，\n"
+                        f"要准备开始今天的工作了！！\n"
+                        "\n"
+                        "去工位前要记得打卡哦~"
+                    )
+                ],
             title = "上班打卡提醒~"
         )
     )
@@ -103,13 +105,15 @@ def end_remind():
     bot.send(
         Message(
                 [
-                MessageSegment.text(
-                    f"已经是{get_china_time()}了，\n"
-                    "工作不要忘了时间√\n"
-                    "\n"
-                    "下班要记得打卡哦~"
-                )
-            ],
+                    MessageSegment.at("all"),
+                    MessageSegment.delimiter(),
+                    MessageSegment.text(
+                        f"已经是{get_china_time()}了，\n"
+                        "工作不要忘了时间√\n"
+                        "\n"
+                        "下班要记得打卡哦~"
+                    )
+                ],
             title = "临下班打卡提醒~"
         )
     )
